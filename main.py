e73a09170aed31d4dd96104fc4f09aa1fe5daffb
Appúchia <46938448+appuchias@users.noreply.github.com>
2020-07-19 13:24:36 +0200

    Moved everything inside a class

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 import sys, os, json
 
-default_folder = ""
+class FileSort():
+    default_folder = ""
 
-def get_dir_names(dir):
-    return [folder.name for folder in os.scandir(dir)]
+    def get_dir_names(dir):
+        return [folder.name for folder in os.scandir(dir)]
 
+    def get_file_type(file):
+        name = file.name
+        extension = name.split(".")[-1:][0]
 
-def get_file_type(file):
-    name = file.name
-    extension = name.split(".")[-1:][0]
-    
-    with open("relations.json") as r:
-        relations = json.load(r)
-    
-    for type in relations:
-        if extension in relations[type]:
-            return type
-    
-    print(f"Unknown ({extension}) - {name}")
-    return "Unknown"
+        with open("relations.json") as r:
+            relations = json.load(r)
+        
+        for type in relations:
+            if extension in relations[type]:
+                return type
+        
+        print(f"Unknown ({extension}) - {name}")
+        return "Unknown"
 
+    def main():
+        if len(sys.argv) > 1:
+            for obj in os.scandir(sys.argv[1]):
+                if obj.is_file():
+                    print(obj.name + " -> " + get_file_type(obj))
+        else:
+            assert len(default_folder) > 0, "Please specify a default folder inside 'main.py'"
+            for obj in os.scandir(default_folder):
+                if obj.is_file():
+                    print(obj.name + " -> " + get_file_type(obj))
 
-if len(sys.argv) > 1:
-    for obj in os.scandir(sys.argv[1]):
-        if obj.is_file():
-            print(obj.name + " -> " + get_file_type(obj))
-else:
-    assert len(default_folder) > 0, "Please specify a default folder inside 'main.py'"
-    for obj in os.scandir(default_folder):
-        if obj.is_file():
-            print(obj.name + " -> " + get_file_type(obj))
+if __name__ == "__main__":
+    FileSort.main()
