b612e93d7846e74fbed844ab9ca9ec6067e33f02
Appúchia <46938448+appuchias@users.noreply.github.com>
2020-07-20 16:53:51 +0200

    More file extensions. Folders are now moved into Folders"

--- main.py
+++ main.py
@@ -1,35 +1,60 @@
-import sys, os, json
+import sys, os, json, shutil
+from os import path
 
 class FileSort():
     default_folder = ""
+    
+    with open("relations.json") as r:
+            relations = json.load(r)
 
-    def get_dir_names(dir):
-        return [folder.name for folder in os.scandir(dir)]
+    # Get everything inside the folder
+    def get_dir_names(self, _dir):
+        return [folder.name for folder in os.scandir(_dir)]
 
-    def get_file_type(file):
+    # Get the file type through its extension
+    def get_file_type(self, file):
         name = file.name
         extension = name.split(".")[-1:][0]
-
-        with open("relations.json") as r:
-            relations = json.load(r)
         
-        for type in relations:
-            if extension in relations[type]:
-                return type
+        for filetype in self.relations:
+            if extension in self.relations[filetype]:
+                return filetype
         
         print(f"Unknown ({extension}) - {name}")
         return "Unknown"
 
-    def main():
+    # Main functionality
+    def main(self):
         if len(sys.argv) > 1:
-            for obj in os.scandir(sys.argv[1]):
-                if obj.is_file():
-                    print(obj.name + " -> " + get_file_type(obj))
+            folder = sys.argv[1]
         else:
-            assert len(default_folder) > 0, "Please specify a default folder inside 'main.py'"
-            for obj in os.scandir(default_folder):
-                if obj.is_file():
-                    print(obj.name + " -> " + get_file_type(obj))
+            assert len(self.default_folder) > 0, "Please specify a default folder inside 'main.py'" # Make sure it can run
+            folder = self.default_folder
+        
+        for obj in os.scandir(folder):
+            if obj.is_dir():
+                if obj.name not in self.relations.keys() and obj.name != "Folders": # Avoid moving any unwanted folder
+                    filepath = "\\".join(obj.path.split("\\")[:-1]) + "\\" # Remove the folder name from the path
+                    assert path.isdir(filepath) # Avoid errors
+                    
+                    if not os.path.exists(filepath +"Folders\\"): # Create the "Folders" folder in case it doesn't exist
+                        os.mkdir(filepath + "Folders\\")
+                    
+                    print(obj.name + " -> " + "Folders\\" + obj.name)
+                    os.rename(obj.path, filepath + "Folders\\" + obj.name) # Move the folder into the "Folders" folder
+                
+                else:
+                    print(f"Omitting \"{obj.name}\" folder")
+            
+            elif obj.is_file():
+                filetype = self.get_file_type(obj)
+                filepath = "\\".join(obj.path.split("\\")[:-1]) + "\\" # Remove the file name from its path
+                assert path.isdir(filepath) # Avoid errors
+                if not os.path.exists(filepath + filetype + "\\"): # Create the subfolder for the file type
+                    os.mkdir(filepath + filetype + "\\")
+                print(obj.name + " -> " + filetype)
+                os.rename(obj.path, filepath + filetype + "\\" + obj.name)
 
 if __name__ == "__main__":
-    FileSort.main()
+    file_sort = FileSort()
+    file_sort.main()
